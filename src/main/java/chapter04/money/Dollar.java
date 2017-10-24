6b0008ceee8f5cf9ec2964c47f55aa58e3be379a
makopi23 <kipersonalpc@gmail.com>
2017-10-25 00:09:31 +0900

    P.28 commit.

---
+++ src/main/java/chapter04/money/Dollar.java
@@ -0,0 +1,18 @@
+package chapter04.money;
+
+public class Dollar {
+	private int amount;
+	
+	Dollar(int amount){
+		this.amount = amount;
+	}
+	
+	Dollar times(int multiplier) {
+		return new Dollar(amount * multiplier);
+	}
+	
+	public boolean equals(Object object) {
+		Dollar dollar = (Dollar)object;
+		return amount == dollar.amount;
+	}
+}
