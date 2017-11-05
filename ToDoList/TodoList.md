a14c352f371216418b7a9f5be857284142c21e12
makopi23 <kipersonalpc@gmail.com>
2017-11-05 21:28:05 +0900

    P.46 commit.

---
+++ ToDoList/TodoList.md
@@ -0,0 +1,2 @@
+以下で管理する。  
+https://github.com/makopi23/TddByExample/pull/1
--- src/main/java/chapter07/money/Money.java
+++ src/main/java/chapter07/money/Money.java
@@ -5,6 +5,7 @@ public class Money {
 	
 	public boolean equals(Object object) {
 		Money money = (Money)object;
-		return amount == money.amount;
+		return amount == money.amount
+				&& getClass().equals(money.getClass());
 	}
 }
