938d899558c5b859b63f2cf48a7aa67cfafcee17
makopi23 <kipersonalpc@gmail.com>
2017-12-07 18:22:14 +0900

    P.107 commit.

--- src/main/java/chapter14/money/Bank.java
+++ src/main/java/chapter14/money/Bank.java
@@ -1,15 +1,19 @@
 package chapter14.money;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Bank {
+	private Map<Pair, Integer> rates = new HashMap<>();
 	Money reduce(Expression source, String to) {
 		return source.reduce(this, to);
 	}
 	
 	void addRate(String from, String to, int rate) {
-		
+		rates.put(new Pair(from, to), rate);
 	}
 	
 	int rate(String from, String to) {
-		return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+		return rates.get(new Pair(from, to));
 	}
 }
