5fe5e10fea7e8d2357cb454c6ff13e057c63b00a
makopi23 <kipersonalpc@gmail.com>
2017-12-15 00:43:01 +0900

    P.118 commit.

--- src/main/java/chapter15/money/Expression.java
+++ src/main/java/chapter15/money/Expression.java
@@ -1,5 +1,6 @@
 package chapter15.money;
 
 interface Expression {
+	Expression plus(Expression addend);
 	Money reduce(Bank bank, String to);
 }
--- src/main/java/chapter15/money/Sum.java
+++ src/main/java/chapter15/money/Sum.java
@@ -1,11 +1,14 @@
 package chapter15.money;
 
 class Sum implements Expression{
-	Money augend;
-	Money addend;
-	Sum(Money augend, Money addend){
+	Expression augend;
+	Expression addend;
+	Sum(Expression augend, Expression addend){
 		this.augend = augend;
 		this.addend = addend;
+	}
+	public Expression plus(Expression addend) {
+		return null;
 	}
 	public Money reduce(Bank bank, String to) {
 		int amount = augend.reduce(bank, to).amount 
