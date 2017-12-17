e09a68d93f3df4731a3c169c5d239f0044685ec3
makopi23 <kipersonalpc@gmail.com>
2017-12-17 22:06:42 +0900

    Chapter18 finished.

--- src/main/python/xunit.py
+++ src/main/python/xunit.py
@@ -1,11 +1,21 @@
-class WasRun:
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
+        super().__init__(name)
     def testMethod(self):
         self.wasRun = 1
-    
-test = WasRun("testMethod")
-print(test.wasRun)
-test.testMethod()
-print(test.wasRun)
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+TestCaseTest("testRunning").run()
 
