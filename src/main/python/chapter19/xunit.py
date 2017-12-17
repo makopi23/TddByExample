f5b7f93e1ad46123f02b5bdb6410e8b8f9d8d548
makopi23 <kipersonalpc@gmail.com>
2017-12-17 23:42:57 +0900

    Chapter19 finished.

---
+++ src/main/python/chapter19/xunit.py
@@ -0,0 +1,30 @@
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def setUp(self):
+        pass
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+        
+class WasRun(TestCase):
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp = 1
+    def testMethod(self):
+        self.wasRun = 1
+        
+class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+    def testRunning(self):
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
+
