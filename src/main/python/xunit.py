5619acfe9d3350812591bf064f2cb31706904be2
makopi23 <kipersonalpc@gmail.com>
2017-12-17 21:35:58 +0900

    P.145 commit.

--- src/main/python/xunit.py
+++ src/main/python/xunit.py
@@ -1,8 +1,11 @@
 class WasRun:
-    pass
-
+    def __init__(self, name):
+        self.wasRun = None
+    def testMethod(self):
+        self.wasRun = 1
+    
 test = WasRun("testMethod")
 print(test.wasRun)
 test.testMethod()
-print(test.WasRun)
+print(test.wasRun)
 
