bf8ca6884102b26f15062ccc3cb20953b71f1bcd
makopi23 <kipersonalpc@gmail.com>
2018-01-16 01:22:18 +0900

    P.164 commit.

---
+++ src/main/python/chapter21/xunit.py
@@ -0,0 +1,40 @@
+class TestResult:
+        def summary(self):
+            return "1 run, 0 failed"
+
+class TestCase:
+    def __init__(self, name):
+        self.name = name
+    def setUp(self):
+        pass
+    def tearDown(self):
+        pass
+    def run(self):
+        self.setUp()
+        method = getattr(self, self.name)
+        method()
+        self.tearDown()
+        return TestResult()
+        
+class WasRun(TestCase):
+    def setUp(self):
+        self.log = "setUp "
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+        
+class TestCaseTest(TestCase):
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+
+print("Test Start!")
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+print("Test Finished!")
